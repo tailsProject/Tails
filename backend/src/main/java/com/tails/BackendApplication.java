9cd1bba20f4ae01c7e576e73cd07ff2725aa3763
1gyeolpark <marine3682@gmail.com>
2026-07-06 18:22:05 +0900

    chore: 패키지 구조 설정

--- backend/src/main/java/com/tails/BackendApplication.java
+++ backend/src/main/java/com/tails/BackendApplication.java
@@ -1,9 +1,11 @@
-package com.tails.backend;
+package com.tails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BackendApplication {
 
 	public static void main(String[] args) {
