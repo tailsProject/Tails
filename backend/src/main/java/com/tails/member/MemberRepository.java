5b4c47ba6ce1eb41ba9c94f7104be0c8f8066422
1gyeolpark <marine3682@gmail.com>
2026-07-07 17:09:52 +0900

    docs: 역할 설명 주석 추가

--- backend/src/main/java/com/tails/member/MemberRepository.java
+++ backend/src/main/java/com/tails/member/MemberRepository.java
@@ -2,6 +2,7 @@ package com.tails.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// 회원 데이터 관리 JPA Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
