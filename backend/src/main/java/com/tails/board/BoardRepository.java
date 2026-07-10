62971f969a8654ed55a8b597f5710f298bce483a
1gyeolpark <marine3682@gmail.com>
2026-07-10 10:22:10 +0900

    fix: 게시글 목록 조회 시 발생하는 N+1 쿼리 문제 수정

--- backend/src/main/java/com/tails/board/BoardRepository.java
+++ backend/src/main/java/com/tails/board/BoardRepository.java
@@ -1,5 +1,7 @@
 package com.tails.board;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +14,8 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @Modifying
     @Query("update Board b set b.viewCount = b.viewCount + 1 where b.id = :id")
     void increaseViewCount(@Param("id") Long id);
+
+    @Query(value = "select b from Board b left join fetch b.member",
+            countQuery = "select count(b) from Board b")
+    Page<Board> findAllWithMember(Pageable pageable);
 }
